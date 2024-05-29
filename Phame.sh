c0cba25768f9a9a8746db229901d245f0788bede
Yi Huang <winniendy@gmail.com>
2024-05-29 10:21:02 -0400

    Create Phame.sh

---
+++ Phame.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=bam_to_fasta_batch
+#SBATCH --qos=bphl-umbrella
+#SBATCH --account=bphl-umbrella
+#SBATCH --output=phame.%j.out 
+#SBATCH --error=phame.err
+#SBATCH --ntasks=1                       
+#SBATCH --cpus-per-task=10
+#SBATCH --time=40:00:00                   
+#SBATCH --mem=100gb                          
+#SBATCH --mail-user=yi.huang@flhealth.gov
+#SBATCH --mail-type=FAIL,END
+
+module load phame
+phame complete_phame.ctl
